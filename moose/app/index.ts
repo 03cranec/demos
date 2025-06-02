b55d5b4924a404db804ee3fd15186f761a57e3ca
armanjindal <arman.jindal@hey.com>
2025-06-01 23:01:15 -0400

    initial cdc demo refactor

--- moose/app/index.ts
+++ moose/app/index.ts
@@ -1,8 +1,8 @@
 import { IngestPipeline } from "@514labs/moose-lib";
 import { uk_price_paid } from "./datamodels/UKHousingPricing";
 
-export const UkPricePaidPipeline = new IngestPipeline<uk_price_paid>("uk_price_paid", {
-    table: true,
-    stream: true,
-    ingest: true,
-});
+// export const UkPricePaidPipeline = new IngestPipeline<uk_price_paid>("uk_price_paid", {
+//     table: true,
+//     stream: true,
+//     ingest: true,
+// });
