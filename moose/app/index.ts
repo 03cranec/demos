ecb91ba35a866dcbd5e80559014724369836b0d7
armanjindal <arman.jindal@hey.com>
2025-06-02 00:18:17 -0400

    reinit the infra

--- moose/app/index.ts
+++ moose/app/index.ts
@@ -1,10 +1,10 @@
 import { IngestPipeline } from "@514labs/moose-lib";
 
-// export * from "./datamodels/UKHousingPricing";
+export * from "./datamodels/UKHousingPricing";
 
-// // Materialized Views - running average and max price
-// export * from "./views/materializedRunningAverage";
+// Materialized Views - running average and max price
+export * from "./views/materializedRunningAverage";
 
-// // APIs - UK Aggregated Prices
-// export * from "./apis/ukAggregatedPricesApi";
+// APIs - UK Aggregated Prices
+export * from "./apis/ukAggregatedPricesApi";
 
